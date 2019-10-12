90ba63cd6c277ece79fd8535b596afec2eb4970a
fijolek.mm@gmail.com <Fijolek Michal>
2019-10-12 15:39:01 +0200

    implementation main and test class

---
+++ src/test/java/eu/szestkam/cryptotraining/rot_13/Rot13Test.java
@@ -0,0 +1,103 @@
+package eu.szestkam.cryptotraining.rot_13;
+
+import eu.szestkam.cryptotraining.cesar.CesarCipher;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Rot13Test {
+
+    private static final String SAMPLE_PHRASE = "sample";
+    private Rot13 cipher13NoArg;
+    private Rot13 cipher13Phrase;
+
+    @BeforeEach
+    void setUp() {
+        cipher13NoArg = new Rot13();
+        cipher13Phrase = new Rot13(SAMPLE_PHRASE);
+    }
+
+    @DisplayName("Correct cipher instance with no args")
+    @Test
+    public void testIfCesarExist() {
+        Rot13 cipher13 = cipher13NoArg;
+        assertNotNull(cipher13);
+    }
+
+    @DisplayName("Correct cipher instance with phrase")
+    @Test
+    public void testPassStringToConstructor() {
+        Rot13 cipher13 = cipher13Phrase;
+        assertNotNull(cipher13);
+    }
+
+    @Test
+    public void testEncrypWhenNoTextThenEmptyString() {
+        Rot13 cipher13 = cipher13NoArg;
+        String encoded13 = cipher13.encode13();
+
+        assertNotNull(encoded13);
+        assertEquals("", encoded13);
+    }
+
+    @Test
+    public void testEncrypWhenTextThenSameLenghtOfString() {
+        Rot13 cipher13 = cipher13Phrase;
+        String encoded13 = cipher13.encode13();
+
+        assertNotNull(encoded13);
+        assertEquals(SAMPLE_PHRASE.length(), encoded13.length());
+    }
+
+    @Test
+    public void testSimpleEncodeLowerCase() {
+        Rot13 cipher13 = new Rot13("abc");
+        String code13 = "mno";
+        String encoded13 = cipher13.encode13();
+        assertEquals(code13, encoded13);
+
+        cipher13 = new Rot13("xyz");
+        code13 = "klm";
+        encoded13 = cipher13.encode13();
+        assertEquals(code13, encoded13);
+    }
+
+    @Test
+    public void testSimpleEncodeMixedCase() {
+        Rot13 cipher13 = new Rot13("abC");
+        String code13 = "mnO";
+        String encoded13 = cipher13.encode13();
+        assertEquals(code13, encoded13);
+
+        cipher13 = new Rot13("XYz");
+        code13 = "KLm";
+        encoded13 = cipher13.encode13();
+        assertEquals(code13, encoded13);
+    }
+
+    @Test
+    public void testDecrypWhenNoTextThenEmptyString() {
+        Rot13 cipher13 = cipher13NoArg;
+        String decoded13 = cipher13.decrypt13();
+
+        assertNotNull(decoded13);
+        assertEquals("", decoded13);
+    }
+    @Test
+    public void testSimpleDecodeMixedCase() {
+        Rot13 cipher13 = new Rot13("mnO");
+        String code13 = "abC";
+        String encoded13 = cipher13.decrypt13();
+        assertEquals(code13, encoded13);
+
+        cipher13 = new Rot13("KLm");
+        code13 = "XYz";
+        encoded13 = cipher13.decrypt13();
+        assertEquals(code13, encoded13);
+
+    }
+
+
+}
